f6f60db6cab2e8c09c1317a4abcbf3204fb71f95
ulitka223 <moonriver7609@gmail.com>
2025-05-11 21:33:19 +0200

    Initializiation and start a simulation

--- main.c
+++ main.c
@@ -6,13 +6,14 @@
 /*   By: dekravts <dekravts@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 22:53:19 by dekravts          #+#    #+#             */
-/*   Updated: 2025/04/29 08:15:11 by dekravts         ###   ########.fr       */
+/*   Updated: 2025/05/11 21:32:31 by dekravts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 #define USAGE "number, die_time, eat_time, sleep_time [number of meals]\x1b[0m"
+
 
 static void	threads_init(t_stol *stol)
 {
